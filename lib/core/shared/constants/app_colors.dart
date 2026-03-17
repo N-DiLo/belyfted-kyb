2c36db0da63d4cbb5665ecb28f436c3de7513a44
DiLo <65601832+N-DiLo@users.noreply.github.com>
2026-03-17 15:22:25 +0100

    chore (feat): added app colors

---
+++ lib/core/shared/constants/app_colors.dart
@@ -0,0 +1,48 @@
+import 'package:belyfted/belyfted.dart';
+
+class AppColors {
+  static const whiteColor = Colors.white;
+  static const darkColor = Color(0xff000000);
+  static const scaffoldBackgroundColor = Color(0xffF9F9F9);
+  static const belyftedInactiveColor = Color(0xffB1BCCB);
+  static const belyftedInactiveTextColor = Color(0xffE6E9EE);
+
+  static const belyftedGreenColor = MaterialColor(0xff22C45E, {
+    50: Color(0xffEFFCF3),
+    800: Color(0xff166534),
+  });
+
+  static const belyftedBlueColor = MaterialColor(0xff1E40AE, {
+    50: Color(0xffEEF5FE),
+    800: Color(0xff1E40AE),
+  });
+
+  static const belyftedPurpleColor = MaterialColor(0xff6B21A7, {
+    50: Color(0xffF9F4FE),
+    800: Color(0xff6B21A7),
+  });
+
+  static const belyftedPrimaryColor = MaterialColor(0xff042656, {
+    0: Color(0xffF4F8FE),
+    50: Color(0xffDCE3ED),
+    100: Color(0xffEEF5FC),
+  });
+
+  static const belyftedNeutralColor = MaterialColor(0xff5A5A5A, {
+    100: Color(0xffF4F4F4),
+    200: Color(0xffE4E4E4),
+    500: Color(0xff737373),
+    800: Color(0xff262626),
+  });
+
+  static const belyftedAmberColor = MaterialColor(0xff91400E, {
+    50: Color(0xffFEFAEA),
+    800: Color(0xff91400E),
+  });
+
+  static const belyftedRedColor = MaterialColor(0xffB81C1C, {
+    100: Color(0xffFDE1E1),
+    700: Color(0xffB81C1C),
+    800: Color(0xff981B1B),
+  });
+}
